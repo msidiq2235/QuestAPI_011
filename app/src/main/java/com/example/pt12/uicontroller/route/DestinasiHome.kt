5261b98a2fe4418ad8b5b2e64d338e4eb19af457
msidiq2235 <muhsidiq2005@gmail.com>
2025-12-20 00:31:27 +0700

    membuat file DestinasiHome.kt dan mengisi code nya

---
+++ app/src/main/java/com/example/pt12/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.pt12.uicontroller.route
+
+import com.example.pt12.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
